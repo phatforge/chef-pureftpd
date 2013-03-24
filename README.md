7db2837cea0ea086d711eeb665784cf84f0ed2eb
Pritesh <pritesh@phatforge.com>
2013-03-24 11:57:52 +0000

    Initial commit of Berkshelf generated cookbook

---
+++ README.md
@@ -0,0 +1,13 @@
+# chef-pureftpd cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: YOUR_NAME (<YOUR_EMAIL>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             "chef-pureftpd"
+maintainer       "YOUR_NAME"
+maintainer_email "YOUR_EMAIL"
+license          "All rights reserved"
+description      "Installs/Configures chef-pureftpd"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue "0.1.0"
+
